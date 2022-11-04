1036f2d7ba46e1bf9bf386a1e8c289d257842c47
Rohit verma <28vrohit@gmail.com>
2022-11-04 17:37:50 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Clock from "./Components/Clock";
+import Timer from "./Components/Timer";
+
+function App() {
+  return (
+    <div className="App">
+      <Timer/>
+      <Clock/>
+    </div>
+  );
+}
+
+export default App;
